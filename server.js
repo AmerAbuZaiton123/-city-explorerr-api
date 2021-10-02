895e56d03ca17da28e9ece2ed7060f0f50929255
AmerAbuZaiton123 <amerabuzaiton@outlook.com>
2021-10-02 14:10:02 +0300

    lab10

--- server.js
+++ server.js
@@ -3,40 +3,42 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const weather = require('./data/weather.json');
+const weather = require('./Modules/Weather.js');
+const movies = require('./Modules/Movies.js')
 const PORT = process.env.PORT;
-let wData= [];
-let weatherObj=[];
+// let wData= [];
+// let weatherObj=[];
 
 app.use(cors());
 
-let getWeather =  (req , res) =>{
-  // let {lat , lon , searchQuery} = req.query;
-  const lat = req.query.lat;
-  const lon = req.query.lon;
-  const searchQuery = req.query.searchQuery;
+// let getWeather =  (req , res) =>{
+//   // let {lat , lon , searchQuery} = req.query;
+//   const lat = req.query.lat;
+//   const lon = req.query.lon;
+//   const searchQuery = req.query.searchQuery;
 
 
-  (lat && lon && searchQuery) ? (wData = weather.find(
-    data => {
-      return data.city_name === searchQuery;
+//   (lat && lon && searchQuery) ? (wData = weather.find(
+//     data => {
+//       return data.city_name === searchQuery;
 
-    }
-  ),
-  weatherObj =wData.data.map(
-    data => new Forecast(data.valid_date , data.weather.description)
-  ),
-  res.status(200).json(weatherObj)
-  )
-    :
-    res.status(500).send(' error ');
-};
+//     }
+//   ),
+//   weatherObj =wData.data.map(
+//     data => new Forecast(data.valid_date , data.weather.description)
+//   ),
+//   res.status(200).json(weatherObj)
+//   )
+//     :
+//     res.status(500).send(' error ');
+// };
 
-app.get('/weather' , getWeather);
-class Forecast {
-  constructor(date , desc){
-    this.date = date ,
-    this.desc = desc;
-  }
-}
+// class Forecast {
+//   constructor(date , desc){
+//     this.date = date ,
+//     this.desc = desc;
+//   }
+// }
+app.get('/weather' , weather);
+app.get('/movie',movies);
 app.listen(PORT , ()=> console.log('server port '+PORT));
